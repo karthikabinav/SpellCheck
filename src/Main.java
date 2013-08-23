c5a42256ce5de64240a76428f24798f4baaf2361
Abhiram Ravi <abhiram.supersaiyan@gmail.com>
2013-08-23 18:42:25 +0530

    Dictionary filling complete

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.io.FileNotFoundException;
+
+
+public class Main
+{
+
+	public static void main(String[] args) throws FileNotFoundException
+	{
+		Dictionary my_dictionary = new Dictionary ();
+		my_dictionary.fillDictionary();
+	}
+
+}
